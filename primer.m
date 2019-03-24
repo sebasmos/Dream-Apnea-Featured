8fd89661d1fff3b31ceb51cfcd4331c83c932f4e
Sebastian Cajas Ordoñez <sebasticajas@gmail.com>
2019-03-24 15:54:26 -0500

    'master'

--- primer.m
+++ primer.m
@@ -1,11 +1,14 @@
+
 clc;
 close all;
 clear all;
 addpath('/Users/alejandralandinez/Documents/MATLAB/mcode');
 
-%% Se usara la base de datos challenge/2010/set-a, la seï¿½al a00
-% el comando physionetdb proporciona un listado de las bases de datos
-% disponibles y el enlace a la base de datos
+%% Se usara la base de datos apnea-ecg, bajamos todas las señales de 
+%entrenamiento, las cuales son 8. Estas vienen con toda la informacion, y
+%tiene los siguientes canales:
+%Respiratorios (A,C,N), cardiacos (ECG), y pulsioximetría(SP02)
+
 
 names=cell(8,1);
 for i=1:4
@@ -19,7 +22,9 @@ end
 names{end}='b01er';
 
 N =60000; %declaro esto para sacar 60000 muestras equivalentes a 
-          %aproximadamente 1 minuto de la seÃ±al
+          %aproximadamente 1 minuto de la señal, pues: 100 Hz es la
+          %frecuencia de muestreo, cada segundo tomo 100 muestras entonces
+          %en un minuto tomo 100*60=6000, si sacamos 60000 es una hora
 for i=1:8
     BaseDatos= strcat({'apnea-ecg/'},names{i});
     siginfo = wfdbdesc(BaseDatos{1});
@@ -31,4 +36,32 @@ for i=1:8
     SPO2(:,i)=signal(:,5);
 end
 
+ECG=ECG';
+respA=respA';
+respC=respC';
+respN=respN';
+SPO2=SPO2';
+%% PARA PROPOSITOS DE VISUALIZACION
+t1m=(1:6000)/fs;
+t30s=(1:3000)/fs;
+t30m=(1:30000)/fs;
+t1h=(1:60000)/fs;
 
+figure(1)
+subplot(3,1,1), plot(t30s,respA(1,(1:3000))'),grid on, axis tight
+subplot(3,1,2), plot(t30s,respC(1,(1:3000))'),grid on, axis tight
+subplot(3,1,3), plot(t30s,respN(1,(1:3000))'),grid on, axis tight
+
+figure(2)
+subplot(2,1,1), plot(t30s,ECG(1,(1:3000))'),grid on, axis tight
+subplot(2,1,2), plot(t30s,SPO2(1,(1:3000))'),grid on, axis tight
+
+% 
+% figure(1)
+% subplot(3,1,1), plot(t30s,RespA(1,(1:3000))'),grid on, axis tight
+% subplot(3,1,2), plot(t30s,RespC(1,(1:3000))'),grid on, axis tight
+% subplot(3,1,3), plot(t30s,RespN(1,(1:3000))'),grid on, axis tight
+% 
+% figure(2)
+% subplot(2,1,1), plot(t30s,ECG(1,(1:3000))'),grid on, axis tight
+% subplot(2,1,2), plot(t30s,SpO2(1,(1:3000))'),grid on, axis tight
