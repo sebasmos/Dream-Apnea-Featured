2033dc4a4cd1a991aa4c198815b03faed520135b
Sebastian Cajas Ordoñez <sebasticajas@gmail.com>
2019-03-24 18:14:57 -0500

    RRinterval

--- main.m
+++ main.m
@@ -2,27 +2,82 @@ clc;
 close all;
 clear all;
 addpath('C:\MATLAB2018\MATLAB\mcode\BioMedicine\Dream Abnea\BiomedicasFinal\apnea-ecg');
-
-apnea = load('a01erm.mat');
-apnea = apnea.val;
-ECG = (apnea(1,:)+30)./200;
-RespC = (apnea(2,:)+1903)./20000;
-RespA= (apnea(3,:)+1903)./20000;
-RespN= (apnea(4,:)+1903)./20000;
-SpO2= apnea(5,:);
+% Partition
+m = 3000;
+% STRING MEMORY BANK
+names=cell(8,1);
+for i = 1:4
+    dbNames = strcat({'a0'},int2str(i),{'erm.mat'});
+    names{i}=dbNames;
+end
+for i=1:3
+    string=strcat({'c0'},int2str(i),{'erm.mat'});
+    names{i+4}=string;
+end
+% SAVE IN PHYSICAL UNITS
+for i = 1:7
+    signal      =  load(char(names{i}));
+    signal      =  signal.val;
+    ECG(:,i)    =  (signal(1,:)+30)./200;
+    RespA(:,i)  =  (signal(2,:)+1903)./20000;
+    RespC(:,i)  =  (signal(3,:)+1903)./20000;
+    RespN(:,i)  =  (signal(4,:)+1903)./20000;
+    SpO2(:,i)   =  signal(5,:);
+end
+% NORMALIZATION MIN MAX
+FullECG = (ECG(:,1)-min(ECG(:,1)))./(max(ECG(:,1))-min(ECG(:,1)));
+FullRespC = (RespC(:,2)-min(RespC(:,2)))./(max(RespC(:,2))-min(RespC(:,2)));
+FullRespA = (RespA(:,3)-min(RespA(:,3)))./(max(RespA(:,3))-min(RespA(:,3)));
+FullRespN = (RespN(:,4)-min(RespN(:,4)))./(max(RespN(:,4))-min(RespN(:,4)));
+FullSpO2 = (SpO2(:,5)-min(SpO2(:,5)))./(max(SpO2(:,5))-min(SpO2(:,5)));
 %  Normalization
-ECG = (ECG(1,:)-min(ECG(1,:)))./(max(ECG(1,:))-min(ECG(1,:)));
-RespC = (RespC(1,:)-min(RespC(1,:)))./(max(RespC(1,:))-min(RespC(1,:)));
-RespA = (RespA(1,:)-min(RespA(1,:)))./(max(RespA(1,:))-min(RespA(1,:)));
-RespN = (RespN(1,:)-min(RespN(1,:)))./(max(RespN(1,:))-min(RespN(1,:)));
-SpO2 = (SpO2(1,:)-min(SpO2(1,:)))./(max(SpO2(1,:))-min(SpO2(1,:)));
-%Fs
+% Sample Frequency
 fs = 100;
-ECGa = abs(ECG).^2;
-[PKS1ruido,LOCS1ruido]=findpeaks(ECGa,fs,'MinPeakHeight',max(ECGa)*0.4);
-findpeaks(ECGa,fs,'MinPeakHeight',max(ECGa)*0.35)
+% Square for easier peak detection
+ECGa = abs(FullECG((1:3000),1)).^2;
+
+% [R1,TR1]=findpeaks(ECGa(1:3000),fs,'MinPeakHeight',max(ECGa)*0.4);
+
+[R1,TR1]=findpeaks(ECGa,fs,'MinPeakHeight',0.35);
+
 %% PARA PROPOSITOS DE VISUALIZACION
+t    = [0:m-1]/fs;
 t1m=(1:6000)/fs;
-t30s=(1:3000)/fs;
-t30m=(1:30000)/fs;
-t1h=(1:60000)/fs;
+t30s= [0:3000-1]./fs;
+t5min = (1:30000)/fs;
+t30m=(1:180000)/fs;
+t1h=(1:360000)/fs;
+ figure
+plot(t,ECGa),grid on, axis tight
+hold on
+plot(TR1,R1,'^r'),title('Peaks in ECG'),ylabel('Magnitude'), xlabel('Time (s)'),grid on, axis tight
+legend('ECG', 'R')
+%% INTERVALS RR
+figure
+title('RR intervals')
+RRinterval = diff(TR1);
+plot((1:length(TR1)-1)/60,RRinterval)
+xlabel('Tiempo (min)');
+ylabel('RR (sec)');
+h_RR = get(gca);
+
+%% PULSACIONES
+title('Càlculo de las pulsaciones por minuto')
+plot((1:length(TR1)-1)/60,1./RRinterval*60,...
+    (1:length(TR1)-1)/60,mean(1./RRinterval*60),'ro');
+ylabel('Pulsaciones por minuto')
+
+%% Peaks detecion with wvs
+% There are two keys for using wavelets as general feature detectors:
+% The wavelet transform separates signal components into different frequency 
+% bands enabling a sparser representation of the signal. You can often find 
+% a wavelet which resembles the feature you are trying to detect. The 'sym4'
+% wavelet resembles the QRS complex, which makes it a good choice for QRS
+% detection. To illustrate this more clearly, extract a QRS complex and plot
+% the result with a dilated and translated 'sym4' wavelet for comparison.
+%[mpdict,~,~,longs] = wmpdictionary(numel(ECGa(1:m)),'lstcpt',{{'sym4',3}});
+
+DetrendedECG = Detrending(ECGa,10);
+Medians = ECGa-DetrendedECG;
+figure
+plot(Medians),title('Drift baseline'),axis tight,grid on,xlabel('samples'),ylabel('amplitude')
--- primer.m
+++ primer.m
@@ -3,19 +3,10 @@ clc;
 close all;
 clear all;
 addpath('/Users/alejandralandinez/Documents/MATLAB/mcode');
-
-<<<<<<< HEAD
-%% Se usara la base de datos apnea-ecg, bajamos todas las señales de 
-%entrenamiento, las cuales son 8. Estas vienen con toda la informacion, y
-%tiene los siguientes canales:
-%Respiratorios (A,C,N), cardiacos (ECG), y pulsioximetría(SP02)
-=======
 %% Se usara la base de datos apnea-ecg, bajamos todas las seÃ±ales de 
 %entrenamiento, las cuales son 8. Estas vienen con toda la informacion, y
 %tiene los siguientes canales:
 %Respiratorios (A,C,N), cardiacos (ECG), y pulsioximetrÃ­a(SP02)
->>>>>>> ceca34c8b6231ed847c66ece6b20a39e2b0ec7ce
-
 
 names=cell(8,1);
 for i=1:4
@@ -29,11 +20,6 @@ end
 names{end}='b01er';
 
 N =60000; %declaro esto para sacar 60000 muestras equivalentes a 
-<<<<<<< HEAD
-          %aproximadamente 1 minuto de la señal, pues: 100 Hz es la
-=======
-          %aproximadamente 1 minuto de la seÃ±al, pues: 100 Hz es la
->>>>>>> ceca34c8b6231ed847c66ece6b20a39e2b0ec7ce
           %frecuencia de muestreo, cada segundo tomo 100 muestras entonces
           %en un minuto tomo 100*60=6000, si sacamos 60000 es una hora
 for i=1:8
@@ -66,12 +52,6 @@ subplot(3,1,3), plot(t30s,respN(1,(1:3000))'),grid on, axis tight
 figure(2)
 subplot(2,1,1), plot(t30s,ECG(1,(1:3000))'),grid on, axis tight
 subplot(2,1,2), plot(t30s,SPO2(1,(1:3000))'),grid on, axis tight
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> ceca34c8b6231ed847c66ece6b20a39e2b0ec7ce
 
 % 
 % figure(1)
